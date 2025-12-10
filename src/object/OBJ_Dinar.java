9b06dc91f442c665e2f02ae8199ede14792a6ede
eyannX <eyannnx@gmail.com>
2025-12-10 20:32:00 +0500

    Initial commit

---
+++ src/object/OBJ_Dinar.java
@@ -0,0 +1,27 @@
+package object;
+
+import entity.Entity;
+import main.GamePanel;
+
+public class OBJ_Dinar extends Entity {
+
+    GamePanel gp;
+    public static final String objName = "Bronze Coin";
+    public OBJ_Dinar(GamePanel gp) {
+        super(gp);
+        this.gp = gp;
+
+        type = type_pickupOnly;
+        name = objName;
+        value = 30;
+        down1 = setup("/objects/coin", 32, 32);
+        price = 25;
+    }
+    public boolean use(Entity entity)
+    {
+        gp.playSE(1);
+        gp.ui.addMessage("Coin +" + value);
+        entity.coin += value;
+        return true;
+    }
+}
